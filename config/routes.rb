af4e51cb870b27b4f3a27febe529e77454119d91
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-04-13 14:14:20 +0000

    [create　routing_building_complete]

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,51 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :orders, only: [:edit, :index, :show, :destroy, :create, :update]
+    resources :order_details, only: [:update]
   end
   namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
+    resources :orders, only: [:show, :update]
   end
   namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
+    resources :customers, only: [:index, :show, :edit, :update]
   end
   namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
+    resources :genres, only: [:index, :create, :edit, :update]
   end
   namespace :admin do
-    get 'homes/top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
+  namespace :admin do
+    root to: 'homes#top'
   end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/thanks'
-    get 'orders/index'
-    get 'orders/show'
+  scope module: :public do
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
-  namespace :public do
-    get 'cart_items/index'
+  scope module: :public do
+    resources :orders, only: [:new, :create, :index, :show]
   end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/withdraw_check'
-    get 'customers/withdraw'
+  post "/orders/confirm" => "public/orders#confirm", as: "public_orders_confirm"
+  get "/orders/thanks" => "public/orders#thanks", as: "public_orders_thanks"
+
+  scope module: :public do
+    resources :cart_items, only: [:index, :update, :destroy, :create]
   end
-  namespace :public do
-    get 'sessions/new'
+  delete "/cars_items/destroy_all" => "public/cars_items#destroy_all", as: ""
+
+  scope module: :public do
+    resource :customers, only: [:show, :edit, :update]
   end
-  namespace :public do
-    get 'registrations/new'
+  get "/customers/withdraw_check" => "public/customers#withdraw_check", as: "withdraw_check"
+  patch "/customers/withdraw" => "public/customers#withdraw", as: ""
+
+  scope module: :public do
+    resources :registrations, only: [:new, :create]
   end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
+  scope module: :public do
+    resources :items, only: [:index, :show]
   end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+  root to: 'public/homes#top'
+  get "/about" => "public/homes#about", as: "public_about"
+
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
