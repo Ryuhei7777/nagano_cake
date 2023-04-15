18c3198b6bc439aa2ce6ebb06cb9786e1697415f
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-04-15 04:14:48 +0000

    [ creating_products_screen]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+  email: "ryuhei0820@me.com",## 任意のメールアドレス,
+  password: "222222"## 任意のパスワード
+)
