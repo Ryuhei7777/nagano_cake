3ab8315bd99592e7ad00aaaf01327fd50ae7cb6c
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-04-16 15:12:31 +0000

    [in_products_detail_screen_implementation]

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,13 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_item
   has_many :order_detail
+
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
