7046745e0516199ba66f71a23dbf21e2ab626864
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-04-16 09:50:17 +0000

    [creating_items_list_screen]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,27 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page])
   end
 
   def new
     @item = Item.new
   end
 
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(item.id)
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+  # ストロングパラメータ
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
 end
