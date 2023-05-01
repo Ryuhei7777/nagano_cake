5926ea589885025d0f9cb2f07927a881b9764e2d
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-05-01 11:55:12 +0000

    [product_detail_implementation_complete]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,15 @@
 class Public::CartItemsController < ApplicationController
   def index
   end
+
+  def create
+    @car_titem = CartItem.new(cart_item_params)
+    @car_titem.customer_id = current_customer.id
+    @car_titem.save
+    redirect_to cart_items_path
+  end
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,10 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items= Item.page(params[:page]).per(8)
   end
 
   def show
+    @item = Item.find(params[:id])
+    @items_new = CartItem.new
   end
 end
