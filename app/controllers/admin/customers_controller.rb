713e7a735bcf160c0fb9d9ad3ed17f3b08b6c572
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-05-04 06:26:23 +0000

    [manager_member_info_edit_implementation_complete]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customers = Customer.page(params[:page])
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def show
@@ -8,5 +8,19 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    customer = @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
+  end
+
+private
+  # ストロングパラメータ
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted )
+  end
+
 end
