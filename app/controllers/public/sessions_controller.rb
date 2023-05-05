c6b2d133fe8cbe864e058214be54b12e1cf39ab2
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-05-05 10:02:35 +0000

    [unsubscribe_person_login_authority_function_implementation_complete]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -33,7 +33,8 @@ class Public::SessionsController < Devise::SessionsController
       end
     end
   end
-
+  
+  
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
