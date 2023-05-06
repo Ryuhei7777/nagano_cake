3fc62b8965b3ac8f19642b8071034ea0bcb14111
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-05-06 04:00:42 +0000

    [  ]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,14 +1,31 @@
 class Public::CartItemsController < ApplicationController
+  # before_action :forbid_login_user, {only: [:create]}
+
   def index
     @cart_items = CartItem.all
     @total_cost = 0
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to cart_items_path
+    if customer_signed_in?
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_items = CartItem.all
+      @cart_items.each do |cart_item|
+        if cart_item.item_id == @cart_item.item_id
+          new_amount  = cart_item.amount + @cart_item.amount
+          cart_item.update(amount: new_amount.to_i)
+          @cart_item.delete
+        end
+      end
+      @cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_items = CartItem.all
+      @total_cost = 0
+      flash[:notice] = "新規ユーザ登録もしくはログインをしてください。"
+      render :index
+    end
   end
 
   def update
